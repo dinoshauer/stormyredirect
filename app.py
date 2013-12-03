9476642f8ee8d4e6b1d0149fe69973ca05e36d43
Kasper Jacobsen <k@three65.org>
2013-12-03 18:45:41 +0100

    Initial commit, ready for first field test

---
+++ app.py
@@ -0,0 +1,38 @@
+import string
+import random
+import tornado.ioloop
+import tornado.web
+import tornado.gen
+import tornadoredis
+import msgpack
+from redis import StrictRedis
+
+c = tornadoredis.Client()
+c.connect()
+r = StrictRedis()
+
+class StormyRedirectHandler(tornado.web.RequestHandler):
+	@tornado.web.asynchronous
+	@tornado.gen.engine
+	def get(self, id):
+		key = '%s%s' % (self.request.host, self.request.uri)
+		self.redirect(r.get(key))
+		yield tornado.gen.Task(c.lpush, 'list_%s' % key, msgpack.packb(self.request.headers))
+
+class StormyGeneratorHandler(tornado.web.RequestHandler):
+	@tornado.gen.engine
+	def post(self):
+		digestive = self._randomString()
+		key = '%s/%s' % (self.request.host, digestive)
+
+	def _randomString(self, size=6, chars=string.ascii_lowercase + string.ascii_uppercase + string.digits):
+		return ''.join(random.choice(chars) for x in xrange(size))
+
+application = tornado.web.Application([
+	(r"/generate", StormyGeneratorHandler),
+	(r"/(.*)", StormyRedirectHandler)
+])
+
+if __name__ == "__main__":
+	application.listen(8888)
+	tornado.ioloop.IOLoop.instance().start()
